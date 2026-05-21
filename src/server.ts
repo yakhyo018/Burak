479874c9a73e5aebda114f5ca93f3295fd0c8c2a
Antony(Yahyo) <choriyevyahyo@gmail.com>
2026-05-21 21:48:16 +0900

    BRR: Initial set up

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+console.log("EXucuted");
+
+import moment from "moment";
+
+const currentTime = moment().format("YYYY MM DD");
+console.log(currentTime);
+
+const person: string = "yahyo";
+const count: number = 100;
