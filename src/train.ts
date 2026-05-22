4e05ec7e4fe7b0f8c1b500b0430bf574ec9bf9e8
Antony(Yahyo) <choriyevyahyo@gmail.com>
2026-05-22 17:27:34 +0900

    feat: MIT TASK L

--- src/train.ts
+++ src/train.ts
@@ -1,1 +1,10 @@
-console.log("HEllo world");
+// MIT TASK L
+
+function reverseSentence(txt: string): string {
+  return txt
+    .split(" ")
+    .map((word) => word.split("").reverse().join(""))
+    .join(" ");
+}
+const result = reverseSentence("i like typescript");
+console.log("result:", result);
