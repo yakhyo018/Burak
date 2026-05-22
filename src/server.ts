c673c2f83af3f70236a8a2ecf6d7b4f248b53157
Antony(Yahyo) <choriyevyahyo@gmail.com>
2026-05-22 12:05:45 +0900

    Feat: connect MongoDB via mongoose

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
-console.log("EXucuted");
+import dotenv from "dotenv";
+dotenv.config();
 
-import moment from "moment";
+import mongoose from "mongoose";
 
-const currentTime = moment().format("YYYY MM DD");
-console.log(currentTime);
-
-const person: string = "yahyo";
-const count: number = 100;
+mongoose
+  .connect(process.env.MONGO_URL as string, {})
+  .then((data) => {
+    console.log("MongoDb connection succeed!");
+    const PORT = process.env.PORT ?? 3003;
+  })
+  .catch((err) => {
+    console.log("ERROR on connection MongoDB", err);
+  });
