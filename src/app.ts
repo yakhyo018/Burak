1ff9b7455a8d450f7e2ff9127228d6ffec998945
Antony(Yahyo) <choriyevyahyo@gmail.com>
2026-06-08 13:06:09 +0900

    feat: integrate middleware sessions

--- src/app.ts
+++ src/app.ts
@@ -12,8 +12,27 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(morgan(MORGAN_FORMAT));
 
-/** 2-SESSIONS **/
+import session from "express-session";
+import ConnectMongoDB from "connect-mongodb-session";
 
+const MongoDBStore = ConnectMongoDB(session);
+const store = new MongoDBStore({
+  uri: String(process.env.MONGO_URL),
+  collection: "sessions",
+});
+
+/** 2-SESSIONS **/
+app.use(
+  session({
+    secret: String(process.env.SESSIONS_SECRET),
+    cookie: {
+      maxAge: 1000 * 3600 * 3, //3h
+    },
+    store: store,
+    resave: true,
+    saveUninitialized: true,
+  }),
+);
 /** 3-VIEWS **/
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
