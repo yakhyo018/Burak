5941880e602db63ce3c4dab8e50f984ce5a99a06
Antony(Yahyo) <choriyevyahyo@gmail.com>
2026-06-12 15:52:12 +0900

    fix: getAllProducts api and passing data into ejs

--- src/controllers/product_controller.ts
+++ src/controllers/product_controller.ts
@@ -13,7 +13,10 @@ const productController: T = {};
 productController.getAllProducts = async (req: Request, res: Response) => {
   try {
     console.log("getAllProducts");
-    res.render("product");
+    const data = await productService.getAllProduct();
+    console.log("data:", data);
+
+    res.render("product", { products: data });
   } catch (err) {
     console.log("Error, getAllProducts", err);
     if (err instanceof Errors) res.status(err.code).json(err);
--- src/models/Product.service.ts
+++ src/models/Product.service.ts
@@ -17,6 +17,13 @@ class ProductService {
 
   /** SSR */
 
+  public async getAllProduct(): Promise<Product[]> {
+    const result = await this.productModel.find().exec();
+    if (!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
+    console.log("result:", result);
+    return result;
+  }
+
   public async createNewProduct(input: ProductInput): Promise<Product> {
     try {
       return await this.productModel.create(input);
